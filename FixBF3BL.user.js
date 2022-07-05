2599b6c45fef9238072d1a12518360ac32d45092
George Forse <george@gforse.uk>
2022-07-05 16:14:04 +0100

    Update FixBF3BL.user.js

--- FixBF3BL.user.js
+++ FixBF3BL.user.js
@@ -12,7 +12,7 @@
 
 //438223020
 const old_data = /\/\/eaassets-a.akamaihd.net\/bl-cdn\/cdnprefix\/production-\d+-\d+\/public\/generated\/en_US\/bundle_base_bottombundles_438223020.js/;
-const new_data = "https://rawcdn.githack.com/MrGForse/BF3-Limited-Edition-B2K-Fix/8195622f9f148613d512b1a375eb2cb68f245a27/bundle_base_bottombundles_438223020.js";
+const new_data = "https://rawcdn.githack.com/GeorgeForse/BF3-Limited-Edition-B2K-Fix/8195622f9f148613d512b1a375eb2cb68f245a27/bundle_base_bottombundles_438223020.js";
 
 function injectScript(originalPage) { //Function injectScript replaces the file
     console.log('Performing inection, please wait...');
